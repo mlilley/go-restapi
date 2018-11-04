415a28f6cb2d659513e7ab6d1ab42296f515ff98
mlilley <michael.lilley@gmail.com>
2018-11-04 01:17:04 -0800

    added repo threadsafety

--- thing/repo.go
+++ thing/repo.go
@@ -1,52 +1,72 @@
 package thing
 
-// TODO - make threadsafe
+import (
+	"sync"
+)
 
 type Repo struct {
 	id     int
 	things Things
+	lock   *sync.RWMutex
 }
 
 func NewRepo() *Repo {
-	return &Repo{id: 0, things: Things{}}
+	return &Repo{id: 0, things: Things{}, lock: &sync.RWMutex{}}
 }
 
 func (r *Repo) GetAll() *Things {
-	return &r.things
+	r.lock.RLock()
+	thingsCopy := make(Things, len(r.things))
+	copy(thingsCopy, r.things)
+	r.lock.RUnlock()
+	return &thingsCopy
 }
 
 func (r *Repo) Create(t Thing) *Thing {
+	r.lock.Lock()
 	r.id++
 	t.ID = r.id
-	r.things = append(r.things, t)
+	r.things = append(r.things, t) // copy of t stored
+	r.lock.Unlock()
 	return &t
 }
 
 func (r *Repo) Get(id int) *Thing {
+	r.lock.RLock()
 	for i := range r.things {
 		if r.things[i].ID == id {
-			return &r.things[i]
+			t := r.things[i]
+			r.lock.RUnlock()
+			return &t
 		}
 	}
+	r.lock.RUnlock()
 	return nil
 }
 
 func (r *Repo) Update(id int, t Thing) *Thing {
+	r.lock.Lock()
 	for i := range r.things {
 		if r.things[i].ID == id {
 			r.things[i].Val = t.Val
-			return &r.things[i]
+			tt := r.things[i]
+			r.lock.Unlock()
+			return &tt
 		}
 	}
+	r.lock.Unlock()
 	return nil
 }
 
 func (r *Repo) Delete(id int) bool {
+	r.lock.Lock()
 	for i := range r.things {
 		if r.things[i].ID == id {
 			r.things = append(r.things[:i], r.things[i+1:]...)
+			r.lock.Unlock()
 			return true
 		}
 	}
+	r.lock.Unlock()
 	return false
 }
